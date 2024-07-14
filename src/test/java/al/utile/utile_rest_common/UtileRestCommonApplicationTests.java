33a4eb461d3bfac9bb04249bffe7f3877f1de523
nidodaj <nikoll.dodaj@eng.it>
2024-07-15 00:21:43 +0200

    init

---
+++ src/test/java/al/utile/utile_rest_common/UtileRestCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package al.utile.utile_rest_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtileRestCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
